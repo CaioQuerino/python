106c8faae2c88b68951a7c4c833ea8035f263368
Caio Querino <caioquerino04@gmail.com>
2025-07-31 15:39:07 -0300

    configs do db

---
+++ api_de_autenticacao_com_flask/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from database import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db.init_app(app)
